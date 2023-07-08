0699fcaf25f9b71e58accf99c424f7f44e076c59
Abhijith Dameruppala <74647983+abh1jith@users.noreply.github.com>
2023-07-08 15:28:36 +0530

    Final Changes!

--- app.js
+++ app.js
@@ -7,20 +7,29 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 
 var song = {"title": "Lofi Girl",
-            "link": "https://www.youtube.com/watch?v=jfKfPfyJRdk"};
+            "link": "https://www.youtube.com/watch?v=jfKfPfyJRdk",
+            "platform": "youtube",
+            "user": "Abhijith"};
 
 var songsList = [song];
 
 app.post("/add", function(req, res){
     let song = {"title": req.body.title,
-                "link": req.body.link};
+                "link": req.body.link,
+                "platform": req.body.platform,
+                "user": req.body.user};
     songsList.push(song);
+    // console.log(song.title + " has been added to the playlist.")
     res.redirect("/");
 });
 
 app.post("/remove", function(req, res){
+    console.log(req.body.removeTitle);
+    
     for(var i = 0; i < songsList.length; i++){
         if(songsList[i].title == req.body.removeTitle){
+            console.log(songsList);
+            console.log(req.body.removeTitle + " has been removed from the playlist.");
             const index = i;
             songsList.splice(index, 1);
             console.log(songsList);
