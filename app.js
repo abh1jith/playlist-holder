63282424127f9a47b2770235cbfc8ec4afe31f9c
Abhijith Dameruppala <74647983+abh1jith@users.noreply.github.com>
2023-07-11 03:58:58 +0530

    Connected to the MongoDB Atlas on the server

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const https = require("https");
 const mongoose = require("mongoose");
+
+
+function timeConverter(inp){
+    var hours = 0;
+    var min = 0;
+    var sec = 0;
+    let h = 1;
+    let m = 1;
+    let s = 1;
+        for (var i = 2; i < inp.length; i ++){
+        if(inp[i] == "H"){
+            h = i;
+        }
+        if(inp[i] == "M"){
+            m = i;
+        }
+        if(inp[i] == "S"){
+            s = i;
+        }
+    }
+    for (var i = 2; i < h; i ++){
+        hours = hours * 10 + Number(inp[i])
+    }
+    for (var i = h + 1; i < m; i ++){
+        min = min * 10 + Number(inp[i])
+    }
+    for (var i = m + 1; i < s; i ++){
+        sec = sec * 10 + Number(inp[i])
+    }
+    sec = sec + (hours * 3600) + (min * 60);
+    return sec;
+}
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 
-mongoose.connect("mongodb://127.0.0.1:27017/playlist");
+// mongoose.connect("mongodb://127.0.0.1:27017/playlist");
+mongoose.connect("mongodb+srv://abhijithdameruppala:abhimani12@cluster0.wthch8f.mongodb.net/playlist")
 
 const songSchema = mongoose.Schema({
     title: String,
@@ -60,8 +94,14 @@ app.get("/", function(req, res){
         .catch(err => {
         console.error(err);
     });
-
-    
+    // var vidLink = "YBkfHAVKwNE";
+    // var ytURL = "https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=" + vidLink + "&key=" + ytAPI;
+    // https.get(ytURL, function(response){
+    //     response.on("data", function(data){
+    //         let datainp = JSON.parse(data);
+    //         console.log(timeConverter(datainp.items[0].contentDetails.duration));
+    //     });
+    // });
 });
 
 app.listen(3000, function(err){
